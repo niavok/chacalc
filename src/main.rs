74d21b379d5b8e19574e514aee5fdcb6a4e83ac5
Frédéric Bertolus <frederic.bertolus@parrot.com>
2017-02-03 17:14:31 +0100

    Remove termios

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,11 @@
-extern crate termios;
 extern crate rustyline;
-use std::io;
-use std::io::Read;
-use std::io::Write;
-//use termios::{Termios, TCSANOW, ECHO, ICANON, tcsetattr};
 
 fn main() {
+    // Head line
     println!("Chacalc calculator. Type type a command, an expression or 'help'.");
-    
 
+
+    // Prompt while not exiting
     loop {
         let prompt_result = prompt();
         match prompt_result {
@@ -16,43 +13,7 @@ fn main() {
             Err(_)           => return
         }
     }
-
-    /*loop {
-        print!("> ");
-        io::stdout().flush().ok().expect("Could not flush stdout");
-        
-        io::stdin().read_line(&mut command_line).expect("Failed to read line");
-        println!("ok: {}", command_line);
-    }*/
 }
-/*
-fn prompt() -> String {
-    let stdin = 0; // couldn't get std::os::unix::io::FromRawFd to work 
-                   // on /dev/stdin or /dev/tty
-    let termios = Termios::from_fd(stdin).unwrap();
-    let mut new_termios = termios.clone();  // make a mutable copy of termios 
-                                            // that we will modify
-    new_termios.c_lflag &= !(ICANON | ECHO); // no echo and canonical mode
-    tcsetattr(stdin, TCSANOW, &mut new_termios).unwrap();
-    let stdout = io::stdout();
-    let mut reader = io::stdin();
-    let mut buffer = [0;1];  // read exactly one byte
-
-    // display the prompt
-    print!("> ");
-    stdout.lock().flush().unwrap();
-
-    
-    reader.read_exact(&mut buffer).unwrap();
-    //println!("You have hit: {:?}", buffer);
-    
-
-
-    tcsetattr(stdin, TCSANOW, & termios).unwrap();  // reset the stdin to 
-                                                    // original termios data
-
-    String::new()
-}*/
 
 fn prompt() -> Result<String, ()> {
     let mut rl = rustyline::Editor::<()>::new();
