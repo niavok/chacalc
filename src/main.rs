60e62394a2241c9b07dffe137614f31bff2d4b1b
Frédéric Bertolus <frederic.bertolus@parrot.com>
2017-02-03 17:54:36 +0100

    Begin to implements the completer

--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,43 @@
 extern crate rustyline;
+use rustyline::line_buffer::LineBuffer;
+
+struct ChacalcCompleter {
+
+}
+
 
 fn main() {
     // Head line
     println!("Chacalc calculator. Type type a command, an expression or 'help'.");
 
+    prompt();
 
+}
+
+fn prompt() {
+    let mut rl = rustyline::Editor::<()>::new();
+
+    let completer : ChacalcCompleter;
+
+    rl.set_completer(Some(completer));
     // Prompt while not exiting
     loop {
-        let prompt_result = prompt();
-        match prompt_result {
-            Ok(command_line) => println!("line: {}", command_line),
-            Err(_)           => return
+        let readline = rl.readline("> ");
+        match readline {
+            Ok(command_line) => return println!("line: {}", command_line),
+            Err(_)   => break,
         }
     }
 }
 
-fn prompt() -> Result<String, ()> {
-    let mut rl = rustyline::Editor::<()>::new();
-    let readline = rl.readline("> ");
-    match readline {
-        Ok(line) => return Ok(line),
-        Err(_)   => return Err(()),
+
+
+
+impl rustyline::completion::Completer for ChacalcCompleter {
+    fn complete(&self, line: &str, pos: usize) -> rustyline::Result<(usize, Vec<String>)> {
+    }
+
+    fn update(&self, line: &mut LineBuffer, start: usize, elected: &str) {
     }
 }
+
